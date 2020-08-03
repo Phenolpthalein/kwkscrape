37f5ae458818ae11855056a8193e62afa10dbeb3
Phenolpthalein <36351700+Phenolpthalein@users.noreply.github.com>
2020-08-03 17:45:51 -0400

    Add files via upload

---
+++ scrapecmd3.py
@@ -0,0 +1,24 @@
+import GetOldTweets3 as got
+import csv
+import argparse
+from pathlib import Path
+namedir = str(Path.home())
+parser = argparse.ArgumentParser()
+parser.add_argument('--username', required=True, help='Defines the target account for scraping, @ character is not necessary')
+parser.add_argument('--count', type=int, default=1000, required=False, help='Defines the amount of tweets to be scraped from an account, defaults to 1000 tweets')
+parser.add_argument('--filename', required=True, help='Defines the name of the output file, make sure to include filename; works best with csv or txt.' )
+#scrape parameters
+args = parser.parse_args()
+#username = args.username
+#count = args.count
+#filename = args.filename
+# Creation of query object
+tweetCriteria = got.manager.TweetCriteria().setUsername(args.username)\
+                                        .setMaxTweets(args.count)
+# Creation of list that contains all tweets
+tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+# Creating list of chosen tweet data
+user_tweets = [[tweet.text] for tweet in tweets]
+with open (namedir +'\\Desktop\\' + args.filename, 'w') as file1:
+    wr = csv.writer (file1, quoting=csv.QUOTE_ALL)
+    wr.writerows(user_tweets)
