2ee132fba1e44c4177946f2b3f75f043d5281882
Phenolpthalein <36351700+Phenolpthalein@users.noreply.github.com>
2020-08-03 18:04:03 -0400

    Add files via upload

--- scrapecmd.py
+++ scrapecmd.py
@@ -7,11 +7,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--username', required=True, help='Defines the target account for scraping, @ character is not necessary')
 parser.add_argument('--count', type=int, default=1000, required=False, help='Defines the amount of tweets to be scraped from an account, defaults to 1000 tweets')
 parser.add_argument('--filename', required=True, help='Defines the name of the output file, make sure to include filename; works best with csv or txt.' )
-#scrape parameters
+# Scrape parameters
 args = parser.parse_args()
-#username = args.username
-#count = args.count
-#filename = args.filename
 # Creation of query object
 tweetCriteria = got.manager.TweetCriteria().setUsername(args.username)\
                                         .setMaxTweets(args.count)
@@ -19,6 +16,6 @@ tweetCriteria = got.manager.TweetCriteria().setUsername(args.username)\
 tweets = got.manager.TweetManager.getTweets(tweetCriteria)
 # Creating list of chosen tweet data
 user_tweets = [[tweet.text] for tweet in tweets]
-with open (expanduser +'\\Desktop\\' + args.filename, 'w') as file1:
+with open (expanduser('~') +'\\Desktop\\' + args.filename, 'w') as file1:
     wr = csv.writer (file1, quoting=csv.QUOTE_ALL)
     wr.writerows(user_tweets)
